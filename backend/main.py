2a6163b2d3baa79dab514de74bf88d1219e55b98
ccarluq0911 <ccarluq0911@g.educaand.es>
2025-08-26 14:09:42 +0200

    Preparación de base de datos PostgreSQL en Docker y conexión inicial desde el back-end

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
 @app.get("/")
 def root():
+    engine = create_engine("postgresql+psycopg://postgres:postgres@localhost:5432/inventario")
+    with engine.connect() as connection:
+        print(connection.execute(text("SELECT 1")).fetchall())
     return {"message" : "Backend funciona"}
